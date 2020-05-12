38eeeb95f9667101df53cd0ec1e86a9fd594e2ee
SilviaSha <1398043872@qq.com>
2020-05-12 10:24:14 +0800

    Vue全家桶

---
+++ Vue全家桶/fastfood/src/App.vue
@@ -0,0 +1,38 @@
+<!--页面入口文件-->
+
+<template>
+  <div id="app-wrapper">
+    <transition name="fade">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less">
+  /*fade-enter:进入过渡的开始状态，元素被插入时生效，只应用一帧后立刻删除。*/
+  /*fade-enter-active:进入过渡的结束状态，元素被插入时就生效，在过渡过程完成后移除。*/
+  /*fade-leave:离开过渡的开始状态，元素被删除时触发，只应用一帧后立刻删除。*/
+  /*fade-leave-active:离开过渡的结束状态，元素被删除时生效，离开过渡完成后被删除。*/
+  #app-wrapper {
+    background-color: black;
+    .fade-enter {
+      opacity: 0;
+    }
+    .fade-leave {
+      opacity: 1;
+    }
+    .fade-enter-active {
+      transition: opacity .5s;
+    }
+    .fade-leave-active {
+      opacity: 0;
+      transition: opacity .5s;
+    }
+  }
+</style>
---
+++ Vue全家桶/fastfood/src/pages/Page01.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="page01-wrapper">
+    {{msg}}
+    <hr>
+    <!--<p>$route: {{JSON.toString($route)}}</p>-->
+    <!--<p>$router: {JSON.toString($router)}}</p>-->
+    <p>$route.query: {{$route.query}}</p>
+    <p>新闻1_ID：{{ $route.query.newsId}}</p>
+    <p>新闻1_标题：{{ $route.query.newsTitle}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'page01',
+  data () {
+    return {
+      msg: 'page01-router'
+    }
+  },
+  mounted: function () {
+    console.log(this.$route)
+    console.log(this.$router)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less">
+  .page01-wrapper {
+    background-color: yellow;
+    font-size: 1.5rem;
+  }
+</style>
