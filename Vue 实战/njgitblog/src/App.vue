9172dff5474ddd2508ad2005c5994a69e96c29dd
SilviaSha <1398043872@qq.com>
2020-05-12 10:23:51 +0800

    Vue 实战

---
+++ Vue 实战/njgitblog/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app-window">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app-window {
+  background-color: red;
+  min-width: 1012px;
+}
+</style>
